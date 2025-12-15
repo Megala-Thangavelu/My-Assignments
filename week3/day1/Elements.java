aacb70821c2a113a81fc2d8f84ab605033006e36
Megala-Thangavelu <t.megaladevi@gmail.com>
2025-12-15 20:34:33 +0530

    Add files via upload

---
+++ week3/day1/Elements.java
@@ -0,0 +1,17 @@
+package week3.day1;
+
+public class Elements extends Button {
+	
+	
+
+	public static void main(String[] args) {
+		
+		Button call=new Button();
+		call.click();
+		call.setText(null);
+		call.submit();
+		
+		
+	}
+
+}
