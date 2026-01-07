6ad6a1f6576007db81060f7099efa60a7bd01cda
Megala-Thangavelu <t.megaladevi@gmail.com>
2026-01-07 12:49:31 +0530

    Update BaseClass.java

--- LeafAccountCreation/BaseClass.java
+++ LeafAccountCreation/BaseClass.java
@@ -10,23 +10,6 @@ public class BaseClass extends AbstractTestNGCucumberTests{
 	
 	public static ChromeDriver driver;
 	
-	/*@BeforeMethod
-	public void precondition() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--guest");
-		options.addArguments("--disable-notifications");
-	    driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get("http://leaftaps.com/opentaps/");
-
-	}
-  
-	@AfterMethod
-	public void postCondition() {
-		driver.close();
-
-	}*/
-	
 
 }
+
